7fdb368bc73d98fb5d088b0152a2ae3bee7d336a
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-10-31 15:33:13 -0400

    Add hello world to base route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`listmaker-server listening on port ${port}`)
+})
